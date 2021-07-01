7360db07af08deb95e913b5b82f661ccb4beb1f4
Randy Davoh <randyelorm@yahoo.com>
2021-07-01 13:32:42 +0000

    a few modifications

--- src/Components/likecompo/LikeCompo.jsx
+++ src/Components/likecompo/LikeCompo.jsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 import "./styles.css"
-import Image1 from "../../Images/1.png"
+import Image1 from "../../Images/html.png"
 import Image2 from "../../Images/baby.jpeg"
 import Image3 from "../../Images/css.jpeg"
-import Image4 from "../../Images/chelsea.png"
+import Image4 from "../../Images/indian.jpeg"
 import Image5 from "../../Images/stack.png"
 import Image6 from "../../Images/solve.jpeg"
 import Image7 from "../../Images/hack.jpeg"
@@ -11,21 +11,22 @@ import Image7 from "../../Images/hack.jpeg"
 
 const LikeCompo = () => {
  
-   const [color, setColor] = useState("white")
-   const [correctColor1] = useState("#4d7902")
-   const [WrongColor1] = useState("#99231d")
-   const [correctColor2] = useState("#059142")
-   const [WrongColor2] = useState("#661813")
-   const [correctColor3] = useState("#06c258")
-   const [WrongColor3] = useState("#801e18")
-   const [correctColor4] = useState("#059142")
-   const [WrongColor4] = useState("#4d120e")
-   const [correctColor5] = useState("#004d00")
-   const [WrongColor5] = useState("#99231d")
-   const [correctColor6] = useState("#06c258")
-   const [WrongColor6] = useState("#801e18")
-   const [correctColor7] = useState("#059142")
-   const [WrongColor7] = useState("#99231d")
+   const [firstcolor, setFirstColor] = useState("")
+   const [secondcolor, setSecondColor] = useState("")
+   const [thirdcolor, setThirdColor] = useState("")
+   const [fourthcolor, setFourthColor] = useState("")
+   const [fifthcolor, setFifthColor] = useState("")
+   const [sixthcolor, setSixthColor] = useState("")
+   const [seventhcolor, setSeventhColor] = useState("")
+
+
+   const [score, setScore] = useState (0)
+ 
+
+
+   const [correctColor] = useState("#00E18B")
+   const [WrongColor] = useState("#B00F30")
+
 
 
 
@@ -43,7 +44,8 @@ const LikeCompo = () => {
    const [Correct4, setCorrectChelsea] = useState(" ")
 
  
-   const [Correct5, setCorrectStack] = useState(" ")
+   const [Correct5, setWrongStack] = useState(" ")
+   const [wrong5, setCorrectStack] = useState(" ")
 
    const [wrong6, setWrongSolve] = useState("")
    const [Correct6, setCorrectSolve] = useState(" ")
@@ -57,28 +59,31 @@ const LikeCompo = () => {
             // Truth1
    const truthFunction1 = (event) =>{
           
-            setColor(correctColor1)
-            setCorrect("CORRECT ANSWER!!!")
+            setFirstColor(correctColor)
+            setCorrect("Come for a hug!")
             setWrong(" ")
             event.preventDefault();
+            setScore (score + 1)
    }
 
         // Truth2
    const truthFunction2 = (event) =>{
           
-    setColor(correctColor2)
+    setSecondColor(correctColor)
     setCorrectFace("You know right?. My face was even worse!!!")
     setWrongFace(" ")
     event.preventDefault();
+    setScore (score + 1)
 
 }
 
         // Truth 3
         const truthFunction3 = (event) =>{
                 
-            setColor(correctColor3)
+            setThirdColor(correctColor)
             setCorrectCSS("I LIKE YOU!!!")
             setWrongCSS("")
+            setScore (score + 1)
             event.preventDefault();
         }
 
@@ -86,9 +91,10 @@ const LikeCompo = () => {
         // Truth 4
 
         const truthFunction4 = (event) =>{
-            setCorrectChelsea("BLUE IS THE COLOR!!!")
+            setCorrectChelsea("I tell you!. We are nothing without their 'wideos' ")
             setWrongChelsea("")
-            setColor(correctColor4)
+            setFourthColor(correctColor)
+            setScore (score + 1)
             event.preventDefault();
       }
 
@@ -96,8 +102,10 @@ const LikeCompo = () => {
         // Truth 5
 
         const truthFunction5 = (event) =>{
-            setCorrectStack("I WONT EVEN ALLOW YOU TO LIE ON THIS ONE")
-            setColor(correctColor5)
+            setCorrectStack("Of Course we all do.")
+            setWrongStack (" ")
+            setFifthColor(correctColor)
+            setScore (score + 1)
             event.preventDefault();
       }
 
@@ -107,7 +115,8 @@ const LikeCompo = () => {
         const truthFunction6 = (event) =>{
             setCorrectSolve(" **Fistbump** ")
             setWrongSolve("")
-            setColor(correctColor6)
+            setScore (score + 1)
+            setSixthColor(correctColor)
             event.preventDefault();
       }
 
@@ -115,9 +124,11 @@ const LikeCompo = () => {
         // Truth 7
 
         const truthFunction7 = (event) =>{
-            setCorrectHack(" 'HAHAHAHAHA.. Me too!. **Hugs**' ")
+            setCorrectHack("Me too!. **Hugs** ")
             setWrongHack("")
-            setColor(correctColor7)
+            setSeventhColor(correctColor)
+            setScore (score + 1)
+           
             event.preventDefault();
       }
 
@@ -129,9 +140,10 @@ const LikeCompo = () => {
 
         // wrong1
         const WrongFunction1 = (event) =>{
-            setColor(WrongColor1)
-            setWrong("WRONG ANSWER!!!")
+            setFirstColor(WrongColor)
+            setWrong("Lying Already?")
             setCorrect (" ")
+            setScore (score - 1)
             event.preventDefault(); 
 
         
@@ -139,9 +151,10 @@ const LikeCompo = () => {
 
         // wrong2
         const WrongFunction2 = (event) =>{
-            setColor(WrongColor2)
+            setSecondColor(WrongColor)
             setWrongFace("Stop Lying!!!")
             setCorrectFace ("")
+            setScore (score - 1)
             event.preventDefault(); 
 
         
@@ -149,9 +162,10 @@ const LikeCompo = () => {
 
         // wrong3
         const WrongFunction3 = (event) =>{
-            setColor(WrongColor3)
+            setThirdColor(WrongColor)
             setWrongCSS("LIAR!!!")
             setCorrectCSS(" ")
+            setScore (score - 1)
             event.preventDefault(); 
 
         
@@ -160,9 +174,22 @@ const LikeCompo = () => {
 
         // wrong4
         const WrongFunction4 = (event) =>{
-            setColor(WrongColor4)
-            setWrongChelsea("WHERE IS YOUR CHAMPIOUS LEAGUE TROPHY?")
+            setFourthColor(WrongColor)
+            setWrongChelsea("Thank you pinocchio!")
             setCorrectChelsea(" ")
+            setScore (score - 1)
+            event.preventDefault(); 
+
+        
+        }
+
+
+          // wrong5
+          const WrongFunction5 = (event) =>{
+            setFourthColor(WrongColor)
+            setWrongStack("")
+            setCorrectStack(" ")
+            
             event.preventDefault(); 
 
         
@@ -170,34 +197,37 @@ const LikeCompo = () => {
 
         // wrong6
         const WrongFunction6 = (event) =>{
-            setColor(WrongColor5)
+            setSixthColor(WrongColor)
             setWrongSolve("Yeah Right!!!")
             setCorrectSolve(" ")
+            setScore (score - 1)
             event.preventDefault(); 
 
         
         }
 
         // wrong7
-        const WrongFunction7 = (event) =>{
-            setColor(WrongColor6)
+        const WrongFunction7 = (event) =>{ 
+            setSeventhColor(WrongColor)
             setWrongHack("Okay then. Hack me now with HTML. I dare you!!!")
             setCorrectHack(" ")
+            setScore (score - 1)
             event.preventDefault(); 
 
         
         }
 
 
-
+        
 
 
 
 
     return (
-    <div className ="everything" style= {{ backgroundColor: color }} >
+    <div className ="everything"  >
 
-            <h1 class ="text-center">I AM A LIE DETECTOR</h1>
+            <h1 class ="text-center">HELLO DEVELOPER, WELCOME</h1>
+                    <h2 class ="text-center">I DETECT LIES TOLD BY MOST DEVELOPERS.</h2>
            <h3 className ="text-center">I will change the background color to <span className ="green">GREEN</span>  when you answer correctly and to <span className ="red">RED</span> when you answer wrongly. Good Luck</h3>
            <p className ="text-center">Refresh to Reset the page</p>
 
@@ -205,14 +235,21 @@ const LikeCompo = () => {
                         {/* Thumbnail 1 */}
                     <div className="row">
                     <div className="">
-                        <div className="thumbnail">
-                        <h4 className ="text-center" style ={{color:correctColor1}}>{Correct}</h4>
-                        <h4 className ="text-center" style ={{color:WrongColor1}}>{wrong}</h4>
+                        <div className="thumbnail" style= {{ backgroundColor: firstcolor }}>
+                     
                         <img src={Image1} className = "Images img-responsive" alt="..."/>
                         <div className="caption">
-                            <h3>The above Image is the React Logo</h3>
+                            <h3 > 
+                                Question 1: You feel proud when a non-tech person sees you writing code. 
+                                But deep down you thank God, they don't know that its just HTML.
+                                
+                            </h3>
+
+                           
                            
                             <p><a href="#" className="btn btn-primary True-btn" role="button" onClick={truthFunction1}> TRUE </a> <a href="#" className="btn btn-default False-btn" role="button" onClick={WrongFunction1}>FALSE</a></p>
+                            <h1 className ="text-center" style ={{color:"white"}}>{Correct}</h1>
+                            <h1 className ="text-center" style ={{color:"white"}}>{wrong}</h1>
                         </div>
                         </div>
                     </div>
@@ -221,14 +258,15 @@ const LikeCompo = () => {
                                 {/* Thumbnail 2 */}
                 <div className="row">
                     <div className="">
-                        <div className="thumbnail">
-                        <h4 className ="text-center" style ={{color:correctColor1}}>{Correct2} </h4>
-                        <h4 className ="text-center" style ={{color:WrongColor1}}>{wrong2}</h4>
+                        <div className="thumbnail" style= {{ backgroundColor: secondcolor }} >
+                       
                         <img src={Image2} className ="Images" alt="..."/>
                         <div className="caption">
-                            <h3>This was your face 90% of the time when you were first learning to code</h3>
-                            <p>Hint: Honesty is Key to getting this question right.</p>
+                            <h3> Question 2: This was your face 90% of the time when you were first learning to code</h3>
+                            <h4>Hint: Honesty is Key to getting this question right.</h4>
                             <p><a href="#" className="btn btn- True-btn" role="button" onClick={truthFunction2}>TRUE</a> <a href="#" className="btn btn-default False-btn" role="button"onClick={WrongFunction2}>FALSE</a></p>
+                            <h1 className ="text-center" style ={{color:"white"}}>{Correct2} </h1>
+                            <h1 className ="text-center" style ={{color:"white"}}>{wrong2}</h1>
                         </div>
                         </div>
                     </div>
@@ -237,14 +275,15 @@ const LikeCompo = () => {
                             {/* Thumbnail 3 */}
             <div className="row">
                 <div className="">
-                    <div className="thumbnail">
-                    <h4 className ="text-center" style ={{color:correctColor1}}   >{Correct3}</h4>
-                    <h4 className ="text-center" style ={{color:WrongColor1}}> {wrong3}  </h4>
+                    <div className="thumbnail" style= {{ backgroundColor: thirdcolor }}>
+                    
                     <img src= {Image3} className ="Images" alt="..."/>
                     <div className="caption">
-                        <h3>CSS is fun, easy to understand and loved by every programmer including you.</h3>
-                        <p>Hint: Again, Please dont lie</p>
+                        <h3>Question 3: CSS is fun, easy to understand and loved by every programmer including you.</h3>
+                        <h4>Hint: Again, Please dont lie</h4>
                         <p><a href="#" className="btn btn- True-btn" role="button" onClick={WrongFunction3}>TRUE</a> <a href="#" className="btn btn-default False-btn" role="button" onClick={truthFunction3}>FALSE</a></p>
+                        <h1 className ="text-center" style ={{color:"white"}}   >{Correct3}</h1>
+                        <h1 className ="text-center" style ={{color:"white"}}> {wrong3}  </h1>
                     </div>
                     </div>
                 </div>
@@ -253,14 +292,15 @@ const LikeCompo = () => {
                     {/* Thumbnail 4 */}
                 <div className="row">
                 <div className="">
-                    <div className="thumbnail">
-                    <h4 className ="text-center" style ={{color:correctColor1}}>{Correct4}</h4>
-                    <h4 className ="text-center" style ={{color:WrongColor1}}> {wrong4}  </h4>
+                    <div className="thumbnail" style= {{ backgroundColor: fourthcolor }}>
+           
                     <img src={Image4} className ="Images"alt="..."/>
                     <div className="caption">
-                        <h3>chelsea FC is the best team in the world.</h3>
+                        <h3>Question 4: You watch an inidan on Youtube at least a month.</h3>
                       
-                        <p><a href="#" className="btn btn- True-btn" role="button"  onClick={truthFunction4}>SO TRUE</a> <a href="#" className="btn btn-default False-btn" role="button" onClick={WrongFunction4}>FOR HATERS ONLY</a></p>
+                        <p><a href="#" className="btn btn- True-btn" role="button"  onClick={truthFunction4}>TRUE</a> <a href="#" className="btn btn-default False-btn" role="button" onClick={WrongFunction4}>NEVER</a></p>
+                        <h1 className ="text-center" style ={{color:"white"}}>{Correct4}</h1>
+                        <h1 className ="text-center" style ={{color:"white"}}> {wrong4}  </h1>
                     </div>
                     </div>
                 </div>
@@ -270,13 +310,15 @@ const LikeCompo = () => {
                 {/* Thumbnail 5 */}
                 <div className="row">
                 <div className="">
-                    <div className="thumbnail">
-                    <h4 className ="text-center" style ={{color:correctColor1}}>{Correct5}</h4>
+                    <div className="thumbnail" style= {{ backgroundColor: fifthcolor }}>
+           
                     <img src={Image5} className= "Images"/>
                     <div className="caption">
-                        <h3>You copy code you dont understand from stackoverflow to make your code work</h3>
-                        <p>Hahaha.. Now you cant lie</p>
-                        <p><a href="#" className="btn btn- True-btn" role="button" onClick={truthFunction5}>True</a> <a href="#" className="btn btn-default False-btn" role="button" onClick={truthFunction5}>True Again</a></p>
+                        <h3> Question 5: You copy code you dont understand from stackoverflow to make your code work</h3>
+                        <h4>Hint:The lying button doesn't work on this question. </h4>
+                        <p><a href="#" className="btn btn- True-btn" role="button" onClick={truthFunction5}>AUTOMATICALLY TRUE</a> <a href="#" className="btn btn-default False-btn" role="button" onClick ={WrongFunction5}>LIARS SHOULD CLICK THIS</a></p>
+                        <h1 className ="text-center" style ={{color:"white"}}>{Correct5}</h1>
+                        <h1 className ="text-center" style ={{color:"white"}}>{wrong5}</h1>
                     </div>
                     </div>
                 </div>
@@ -287,14 +329,15 @@ const LikeCompo = () => {
 
                 <div className="row">
                 <div className="">
-                    <div className="thumbnail">
-                    <h4 className ="text-center" style ={{color:correctColor1}}   >{Correct6}</h4>
-                    <h4 className ="text-center" style ={{color:WrongColor1}}> {wrong6}  </h4>
+                    <div className="thumbnail" style= {{ backgroundColor: sixthcolor }}>
+                   
                     <img src={Image6}alt="..."/>
                     <div className="caption">
-                        <h3>You are learning to code because you really love to solve problems and not because of the money</h3>
-                        <p>Hint: Remember your frustrated face when you cant find a simple bug and answer accordingly.</p>
+                        <h3> Question 6: You are learning to code because you really love to solve problems and not because of the money</h3>
+                        <h4>Hint: Remember your frustrated face when you cant find a simple bug and answer accordingly.</h4>
                         <p><a href="#" className="btn btn- True-btn" role="button" onClick={WrongFunction6}>True</a> <a href="#" className="btn btn-default False-btn" role="button" onClick={truthFunction6}>False</a></p>
+                        <h1 className ="text-center" style ={{color:"white"}}>{Correct6}</h1>
+                        <h1 className ="text-center" style ={{color:"white"}}> {wrong6}  </h1>
                     </div>
                     </div>
                 </div>
@@ -304,26 +347,34 @@ const LikeCompo = () => {
                     {/* Thumbnail 7 */}
                 <div className="row">
                 <div className="">
-                    <div className="thumbnail">
-                    <h4 className ="text-center" style ={{color:correctColor1}}   >{Correct7}</h4>
-                    <h4 className ="text-center" style ={{color:WrongColor1}}> {wrong7}  </h4>
+                    <div className="thumbnail" style= {{ backgroundColor: seventhcolor }}>
+                   
                     <img src={Image7}/>
                     <div className="caption">
-                        <h3>You decided to learn how to code because you thought you will be become a hacker but you ended becoming at expert at copying and pasting. </h3>
+                        <h3>Question 7: You decided to learn how to code because you thought you will be become a hacker but you ended becoming at expert at copying and pasting. </h3>
                         <p></p>
                         <p><a href="#" className="btn btn- True-btn" role="button" onClick={truthFunction7}>True</a> <a href="#" className="btn btn-default False-btn" role="button" onClick={WrongFunction7}>False</a></p>
+                        <h1 className ="text-center" style ={{color:"white"}}   >{Correct7}</h1>
+                        <h1 className ="text-center" style ={{color:"white"}}> {wrong7}  </h1>
                     </div>
                     </div>
                 </div>
                 </div>
                 </section>   
 
-            <h2 className ="text-center">THANKS FOR BEING HONEST. IF YOU WERENT, REFRESH AND TRY AGAIN.</h2>
+                <h1 className ="text-center">YOUR IS SCORE: {score} / 7</h1>
+
+               
+
+
+            
             
             
                      
         </div>
     );
+
+    
 }
 
 export default LikeCompo;
